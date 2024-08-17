9780aeade7b6f272f693c2c1671d611b4043ec12
Weverson Godinho <weverson.godinho@icloud.com>
2024-08-17 10:58:54 +0100

    Arrays

--- class53.js
+++ class53.js
@@ -1,28 +1,60 @@
- 
- function myGeeks(){
-    const operand1 = document.getElementById("operand1").value
-     console.log(operand1);
- }
+const result = document.getElementById("result");
+const inputOperand1 = document.getElementById("input-operand1");
+const inputOperand2 = document.getElementById("input-operand2");
+const sum = document.getElementById("sum")
+const sub = document.getElementById("sub");
+const mult = document.getElementById("mult");
+const div = document.getElementById("div");
+
+
 
  const op = [
-   (val) => {
-     let res = 0;
-     for (v of val) {
-       res += v;
-     }
+   (a, b) => {
+     let res = a + b;
      return res;
    },
-   (val) => {
-     let res = 0;
-     for (v of val) {
-       res *= v;
-     }
+   (a, b) => {
+     let res = a - b;
      return res;
    },
-   (val) => {
-     for (v of val) {
-       console.log(v)
-     }
-
-   }
+   (a, b) => {
+     let res = a * b;
+     return res;
+   },
+   (a, b) => {
+     let res = a / b;
+     return res;
+   },
  ];
+
+let operator;
+const getOperator = function () {
+  sum.addEventListener("click", () => {
+    operator = 0;
+    compt()
+  });
+  sub.addEventListener("click", () => {
+    operator = 1;
+    compt();
+  });
+  mult.addEventListener("click", () => {
+    operator = 2;
+    compt();
+  });
+  div.addEventListener("click", () => {
+    operator = 3;
+    compt();
+  });
+};
+
+
+const compt = function(){
+result.addEventListener("click", () => {
+  let result = op[operator](Number(inputOperand1.value), Number(inputOperand2.value));
+  console.log(result);
+});
+}
+
+getOperator();
+
+ 
