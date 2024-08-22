53997f896afe3422997e4f58b80c7ebc3aedb7d4
Weverson Godinho <weverson.godinho@icloud.com>
2024-08-22 22:23:34 +0100

    Calculadora

---
+++ class70.js
@@ -0,0 +1,23 @@
+const teclasNum =  [...document.querySelectorAll('.num')]
+const teclasop = [...document.querySelectorAll(".op")]
+const teclaRes = document.querySelector('.res')
+const display = document.querySelector('.display')
+const ton = document.getElementById('ton')
+const tlimpar = document.getElementById('tlimpar')
+
+teclasNum.forEach((el)=>{
+  el.addEventListener('click', (evt) =>{
+    display.innerHTML += evt.target.innerHTML
+  }) 
+})
+
+
+teclasop.forEach((el) => {
+  el.addEventListener("click", (evt) => {
+    display.innerHTML += evt.target.innerHTML;
+  });
+});
+
+tlimpar.addEventListener('click', (evt) =>{
+  display.innerHTML = '0'
+})
