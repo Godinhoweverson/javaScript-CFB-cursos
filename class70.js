1130f386eefbfa106dd5d9795b960e553412df95
Weverson Godinho <weverson.godinho@icloud.com>
2024-08-24 07:38:10 +0100

    class74 Calculadora

--- class70.js
+++ class70.js
@@ -2,22 +2,74 @@ const teclasNum =  [...document.querySelectorAll('.num')]
 const teclasop = [...document.querySelectorAll(".op")]
 const teclaRes = document.querySelector('.res')
 const display = document.querySelector('.display')
-const ton = document.getElementById('ton')
+const tcpy = document.getElementById('tcpy')
 const tlimpar = document.getElementById('tlimpar')
+const tigual = document.getElementById('tigual')
+const calc_aba = document.getElementById("calc_aba");
+const calc = document.getElementById("calc");
+
+let sinal = false
+let decimal = false
+
 
 teclasNum.forEach((el)=>{
   el.addEventListener('click', (evt) =>{
-    display.innerHTML += evt.target.innerHTML
+    sinal = false
+    
+    if (evt.target.innerHTML === ",") {
+      if (!decimal) {
+        decimal = true;
+        if(display.innerHTML === '0'){
+          display.innerHTML = '0,'
+        }else{
+          display.innerHTML += evt.target.innerHTML;
+        }
+      }
+    } else {
+      if (display.innerHTML === "0") {
+        display.innerHTML = "";
+      }
+      display.innerHTML += evt.target.innerHTML;
+    }
+    
   }) 
 })
 
 
 teclasop.forEach((el) => {
   el.addEventListener("click", (evt) => {
-    display.innerHTML += evt.target.innerHTML;
+    if(!sinal){
+      sinal = true
+      if(display.innerHTML === '0'){
+        display.innerHTML = ""
+      }
+      if(evt.target.innerHTML === 'x'){
+        display.innerHTML += '*'
+      }else{
+        display.innerHTML += evt.target.innerHTML;
+      }
+    }
   });
 });
 
 tlimpar.addEventListener('click', (evt) =>{
+  sinal = false;
+  decimal = false
   display.innerHTML = '0'
 })
+
+tigual.addEventListener("click", (evt) => {
+  sinal = false;
+  decimal = false;
+  const res = eval(display.innerHTML)
+  display.innerHTML = res
+});
+
+
+tcpy.addEventListener('click',(evt)=>{
+  navigator.clipboard.writeText(display.innerHTML)
+})
+
+calc_aba.addEventListener("click", (evt) => {
+  calc.classList.toggle("calc_exibir");
+});
