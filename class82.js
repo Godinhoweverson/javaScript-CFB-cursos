c567e7b28623d619ea3ae5f989ea0affa1beab0e
Weverson Godinho <weverson.godinho@icloud.com>
2024-09-01 07:23:34 +0100

    class 84 Praticas com classes

--- class82.js
+++ class82.js
@@ -1,6 +1,6 @@
 const palco = document.getElementById('palco')
 const num_objetos = document.getElementById("num_objetos");
-const txt_qtde = document.getElementById("txt_qtde ");
+const txt_qtde = document.getElementById("txt_qtde");
 const btn_add = document.getElementById("btn_add");
 const btn_remover= document.getElementById("btn_remover");
 
@@ -25,12 +25,11 @@ class Bola {
       this.arrayBolas = arrayBolas
       this.id = Date.now()+ '_' + Math.floor(Math.random() * 100000000000)
       this.desenhar()
-      this.controle = setInterval(this.constrolar, 10)
+      this.controle = setInterval(this.controlar, 10)
       this.eu = document.getElementById(this.id)
       numBola++
       num_objetos.innerHTML = numBola
     }
-
     minhaPos =() => {
       return this.arrayBolas.indexOf(this)
     }
@@ -42,33 +41,59 @@ class Bola {
           return b
         }
       })
+      this.eu.remove()
+      numBola--
+      num_objetos.innerHTML = numBola;
     }
 
     desenhar = () =>{
-
+      const div = document.createElement('div')
+      div.setAttribute('id', this.id)
+      div.setAttribute('class','bola')
+      div.setAttribute('style',`left:${this.px}px; top: ${this.py}px; width:${this.tam}px; height:${this.tam}px;background-color:rgb(${this.r},${this.g},${this.b})`)
+      this.palco.appendChild(div)
     }
 
-    constrolar = ()=>{
+    controle_bordas = () => {
+      if(this.px +  this.tam >= larguraPalco){
+        this.dirx =-1
+      }else if(this.px <= 0){
+        this.dirx = 1
+      }else  if(this.py +  this.tam >= alturaPalco){
+        this.diry =-1
+      }else  if(this.py <= 0){
+        this.diry = 1
+    }
 
+    controlar = ()=>{
+      this.controle_bordas();
+      this.px += this.dirx * this.velx;
+      this.py += this.diry * this.vely;
+      this.eu.setAttribute('style',`left:${this.px}px; top: ${this.py}px; width:${this.tam}px; height:${this.tam}px;background-color:rgb(${this.r},${this.g},${this.b})`)
+      if((this.px > larguraPalco) || (this.py > alturaPalco)){
+        this.remover()
+      } 
     }
 }
-
+}
 
 window.addEventListener('resize', (evt) => {
- larguraPalco = palco.offsetWidth;
- alturaPalco = palco.offsetHeight;
+  larguraPalco = palco.offsetWidth;
+  alturaPalco = palco.offsetHeight;
 
 })
 
 btn_add.addEventListener('click', (evt) =>{
   const qtde = txt_qtde.value 
   for(let i = 0; i < qtde;i++){
-
+    bolas.push(new Bola(bolas, palco))
   }
 })
+
 
 btn_remover.addEventListener('click', (evt) =>{
   bolas.map((b)=>{
     //Remover a bolinha
+    b.remover()
   })
 })
