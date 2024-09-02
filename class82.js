5d668802c70e9e51921322d7c834527863e1ecbf
Weverson Godinho <weverson.godinho@icloud.com>
2024-09-02 22:15:48 +0100

    fix the error on controle_bordas method

--- class82.js
+++ class82.js
@@ -17,13 +17,13 @@ class Bola {
       this.b = Math.floor(Math.random() * 255);
       this.px = Math.floor(Math.random() * larguraPalco - this.tam);
       this.py = Math.floor(Math.random() * alturaPalco - this.tam);
-      this.velx = Math.floor(Math.random() * 2)+ 0.5;
+      this.velx = Math.floor(Math.random() * 2) + 0.5;
       this.vely = Math.floor(Math.random() * 2) + 0.5;
       this.dirx = Math.floor(Math.random() * 10) > 5 ? 1 : -1;
       this.diry = Math.floor(Math.random() * 10) > 5 ? 1 : -1;
       this.palco = palco
       this.arrayBolas = arrayBolas
-      this.id = Date.now()+ '_' + Math.floor(Math.random() * 100000000000)
+      this.id = Date.now()+'_'+Math.floor(Math.random()*100000000000)
       this.desenhar()
       this.controle = setInterval(this.controlar, 10)
       this.eu = document.getElementById(this.id)
@@ -50,32 +50,37 @@ class Bola {
       const div = document.createElement('div')
       div.setAttribute('id', this.id)
       div.setAttribute('class','bola')
-      div.setAttribute('style',`left:${this.px}px; top: ${this.py}px; width:${this.tam}px; height:${this.tam}px;background-color:rgb(${this.r},${this.g},${this.b})`)
+      div.setAttribute('style',`left:${this.px}px;top:${this.py}px;width:${this.tam}px;height:${this.tam}px;background-color:rgb(${this.r},${this.g},${this.b})`)
       this.palco.appendChild(div)
     }
 
     controle_bordas = () => {
-      if(this.px +  this.tam >= larguraPalco){
+      if(this.px + this.tam >= larguraPalco){
         this.dirx =-1
       }else if(this.px <= 0){
         this.dirx = 1
-      }else  if(this.py +  this.tam >= alturaPalco){
+      } 
+      if(this.py + this.tam >= alturaPalco){
         this.diry =-1
-      }else  if(this.py <= 0){
+      }else if(this.py <= 0){
         this.diry = 1
-    }
+      }
+    }  
 
     controlar = ()=>{
       this.controle_bordas();
       this.px += this.dirx * this.velx;
       this.py += this.diry * this.vely;
-      this.eu.setAttribute('style',`left:${this.px}px; top: ${this.py}px; width:${this.tam}px; height:${this.tam}px;background-color:rgb(${this.r},${this.g},${this.b})`)
+      this.eu.setAttribute(
+        "style",
+        `left:${this.px}px;top:${this.py}px;width:${this.tam}px;height:${this.tam}px;background-color:rgb(${this.r},${this.g},${this.b})`
+      );
       if((this.px > larguraPalco) || (this.py > alturaPalco)){
         this.remover()
       } 
     }
 }
-}
+
 
 window.addEventListener('resize', (evt) => {
   larguraPalco = palco.offsetWidth;
